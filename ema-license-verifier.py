a75acc55352f57a407e9e1ee26b904dc8e1c5b6d
nitrozeus <muhammadas.2021@scis.smu.edu.sg>
2023-04-22 17:08:50 +0800

    implemented gas service worker func

--- ema-license-verifier.py
+++ ema-license-verifier.py
@@ -10,30 +10,49 @@ import re
 import atexit
 
 
-scraped_ids = []
+scraped_electrician_ids = []
+scraped_gas_workers_ids = []
 
 
 def launch_browser():
-    chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--window-size=700,700')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    # browser = webdriver.Chrome(options=chrome_options)
-    # browser = webdriver.Chrome(options=chrome_options, executable_path='/usr/local/bin/chromedriver')
-    browser = webdriver.Chrome(options=chrome_options)
-    browser.get(
-        "https://elise.ema.gov.sg/elise/findworkerservlet?Operation=Get&Item=EL")
-    return browser
+    global browser  # declare browser as a global variable
+    if 'browser' in globals():  # check if browser already exists
+        return browser
+    else:  # create a new browser instance if it doesn't exist
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=700,700')
+        chrome_options.add_experimental_option("detach", True)
+        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        browser = webdriver.Chrome(
+            options=chrome_options, executable_path='/usr/local/bin/chromedriver')  # for macOS/linux, download chromedriver beforehand and place it in /usr/local/bin
+        browser = webdriver.Chrome(options=chrome_options)
+        browser.get(
+            "https://elise.ema.gov.sg/elise/findworkerservlet?Operation=Get&Item=EL")
+        return browser
+
+
+# def launch_browser():
+#     chrome_options = Options()
+#     chrome_options.add_argument('--no-sandbox')
+#     chrome_options.add_argument('--window-size=700,700')
+#     chrome_options.add_argument('--headless')
+#     chrome_options.add_argument('--disable-gpu')
+#     chrome_options.add_argument('--disable-dev-shm-usage')
+#     # browser = webdriver.Chrome(options=chrome_options, executable_path='/usr/local/bin/chromedriver')
+#     browser = webdriver.Chrome(options=chrome_options)
+#     browser.get(
+#         "https://elise.ema.gov.sg/elise/findworkerservlet?Operation=Get&Item=EL")
+#     return browser
 
 
 def bypass_captcha(browser):
     with Halo(text='1. Bypassing CAPTCHA', spinner='dots') as spinner:  # use Halo to display spinner
-        spinner.start()
         captchaImg = browser.find_element(By.ID, "img")
         captchaImg.screenshot('./captchas/captcha.png')
-        api_key = 'YOUR_API_KEY'
+        api_key = 'f41d954ccdabcc0d0462e4b72e1739f5'
         # print(api_key)  # remove this later
         solver = TwoCaptcha(api_key)
         try:
@@ -46,12 +65,12 @@ def bypass_captcha(browser):
             browser.find_element(By.NAME, "captcha").send_keys(code)
             browser.find_element(By.NAME, "cmdSearchByName").click()
         # use Halo to display success message
-        spinner.succeed('Bypass complete')
-        spinner.stop()
+        spinner.succeed('CAPTCHA Bypass complete')
 
 
-def scrape_data(browser):
-    with Halo(text='2. Scraping data...', spinner='dots') as spinner:  # use Halo to display spinner
+def scrape_electrical_worker_data(browser):
+    # use Halo to display spinner
+    with Halo(text='2. Scraping electical worker data...', spinner='dots') as spinner:
         page_source = browser.page_source
         soup = BeautifulSoup(page_source, "lxml")
 
@@ -71,31 +90,76 @@ def scrape_data(browser):
             # remove (hp)
             data = [re.sub(r'\(Hp\)', '', item) for item in data]
             # assume the license ID is the first element in the array
-            scraped_ids.append(data[0])
+            # only show license ID for now
+            scraped_electrician_ids.append(data[0])
 
         # use Halo to display success message
-        spinner.succeed('Scraping complete')
+        spinner.succeed('Scraping electrical worker complete')
+
+
+def scrape_gas_service_worker_data(browser):
+    # use Halo to display spinner
+    with Halo(text='2. Scraping gas service worker data...', spinner='dots') as spinner:
+        page_source = browser.page_source
+        soup = BeautifulSoup(page_source, "lxml")
+
+        rows = soup.find_all("tr", class_="tabledetail")
+        for row in rows:
+            cells = row.find_all("td")
+            data = [cell.get_text(separator='<br>', strip=True).replace(
+                '<br>', ' ') for cell in cells]
+            data.pop(0)  # remove first index
+            # remove email
+            data = [re.sub(r'\S+@\S+', '', item) for item in data]
+            # remove number before (Tel) and (Tel) itself
+            data = [re.sub(r'\d+(?=\(Tel\))\(Tel\)', '', item)
+                    for item in data]
+            # remove any trailing whitespace
+            data = [item.strip() for item in data]
+            # remove (hp)
+            data = [re.sub(r'\(Hp\)', '', item) for item in data]
+            # assume the license ID is the first element in the array
+            scraped_gas_workers_ids.append(data[0])
+
+        # use Halo to display success message
+        spinner.succeed('Scraping gas service worker complete')
 
 
 def scrape():
     # Launch browser
     browser = launch_browser()
-    selectRadioButton = browser.find_element(By.ID, "seachAllRadio")
-    selectRadioButton.click()
+    selectRadioButton = browser.find_element(By.ID, "seachAllRadio").click()
     # Select dropdown for Electrician who offer consumer services
-    selectDropDown = Select(browser.find_element(By.NAME, "WorkerType"))
-    selectDropDown.select_by_value("OFFERE")
+    selectDropDown = Select(browser.find_element(
+        By.NAME, "WorkerType")).select_by_value("OFFERE")
 
-    # Bypass CAPTCHA
+    # Bypass CAPTCHA for Electrician who offer consumer services
     bypass_captcha(browser)
-    # Use beautifulsoup to scrape the data
-    scrape_data(browser)
+    scrape_electrical_worker_data(browser)
+    print("Scraped gas workers:", scraped_electrician_ids)
+
+    # Scrape gas service workers
+    selectTagClass = browser.find_element(
+        By.LINK_TEXT, "Gas Service Worker").click()
+    selectResidentialCheckbox = browser.find_element(
+        By.ID, "chkResidential").click()
+    selectchkNonResidentialCheckbox = browser.find_element(
+        By.ID, "chkNonResidential").click()
+    selectchkAllGasInstallationCheckbox = browser.find_element(
+        By.ID, "chkAllGasInstallation").click()
+
+    # Bypass CAPTCHA for Gas Service Workers who offer consumer services
+    bypass_captcha(browser)
+    scrape_gas_service_worker_data(browser)
+    print("Scraped gas workers:", scraped_gas_workers_ids)
+
     while True:
-        user_input = input("Enter the license ID or type 'q' to quit: ")
+        user_input = input(
+            "\033[33mEnter the license ID or type 'q' to quit: \033[0m")
         if user_input.lower() == 'q':
             break
-        # check scraped_ids for user_input
-        if user_input in scraped_ids:
+        # check scraped_electrician_ids for user_input
+        if user_input in scraped_electrician_ids or user_input in scraped_gas_workers_ids:
             print("\033[32mLicense ID {} found.\033[0m".format(user_input))
         else:
             print("\033[31mLicense ID {} not found.\033[0m".format(user_input))
